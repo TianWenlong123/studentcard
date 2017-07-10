6f0667e49dacfac512a9961bbd87bfa2d2b54efa
田文龙 <twl14@mails.tsinghua.edu.cn>
2017-07-10 11:29:38 +0800

    搭建基本框架

---
+++ cancel/cancel.ino
@@ -0,0 +1,13 @@
+/*
+ * 校园卡注销程序
+ */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
---
+++ store/store.ino
@@ -0,0 +1,12 @@
+/*
+ * 储值
+ */
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
